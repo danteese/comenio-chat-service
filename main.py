9b36d4a35b688e93d286a77c6ccd9d75cd674816
Dante Bazaldua <dalnte@me.com>
2024-06-23 21:17:41 -0600

    feat: version with security but I think is WIP

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-import sqlite3
 from flask import (
     Flask,
     request,
@@ -13,65 +12,61 @@ from llama_index.llms.openai import OpenAI
 from llama_index.core.chat_engine import SimpleChatEngine
 from llama_index.core.llms import ChatMessage, MessageRole
 from dotenv import load_dotenv
+from chatbot.authorization import get_user_has_subscription, verify_jwt, get_user_messages_count
+from chatbot.database import get_db
+from chatbot.constants import comenio_personality, MESSAGE_LIMIT, API_KEY
 
 load_dotenv()
 
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
-print(os.getenv("OPENAI_API_KEY"))
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-comenio_personality = """
-You are Comenio, an assistant with expertise in supporting teachers to enhance the educational experience and improve overall efficiency. Your main objectives are to provide instructional support, facilitate communication, and assist with administrative tasks. Your role is to act as a supportive, reliable, and efficient assistant, helping teachers streamline their tasks and improve the learning environment for their students.
 
-To fulfill your role, you must follow these guidelines:
-
-	1.	Answer the user in the language the user writes to you.
-	2.	Treat all users with respect and avoid making any discriminatory or offensive statements.
-	3.	Swiftly identify the user’s intent and tailor your responses accordingly.
-	4.	If you find that the information at hand is inadequate to fulfill your role and objectives, please ask the user for further information.
-"""
-
-DATABASE = os.getenv("DATABASE_URL")
-
-
-def get_db():
-    db = getattr(g, "_database", None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
-
-
-def is_user_valid():
-    pass  # TODO: Validate token with main service or query database :S
-
-
-def is_conversation_valid(conversation_id) -> bool:
+def is_conversation_valid(conversation_uuid) -> bool:
     """
     Check if the conversation exists in the database
     @param conversation_id: The conversation ID to check
     @return: True if the conversation exists, False otherwise
     """
     cursor = get_db().cursor()
-    cursor.execute("SELECT * FROM conversations WHERE id = ?", (conversation_id,))
+    cursor.execute("SELECT * FROM conversations WHERE uuid = ?", (conversation_uuid,))
     conversations = cursor.fetchone()
     return conversations is not None
 
 
-@app.route("/api/chat/<int:user_id>/<int:conversation_id>/preflight", methods=["POST"])
-def preflight(user_id=None, conversation_id=None):
-    if user_id is None or conversation_id is None:
-        return {"error": "User ID is required"}, 400
+@app.route("/api/chat/<conversation_uuid>/preflight", methods=["POST"])
+def preflight(conversation_uuid=None):
+    if "Authorization" not in request.headers:
+        return {"status": False, "message": "Authorization header is required"}, 403
 
-    # TODO: Implement user validation
-    if not is_conversation_valid(conversation_id):
-        return {"error": "Invalid conversation ID"}, 400
+    token = request.headers.get("Authorization").split(" ")[1]
+    payload = verify_jwt(token)
 
-    print(request.json["message"])
+    if payload is not None:
+        user_id = payload["ur"]
+    else:
+        return {"status": False, "message": "Invalid user ID"}, 403
+
+    if not is_conversation_valid(conversation_uuid):
+        return {"error": "Invalid conversation ID"}, 403
 
     conn = get_db()
     cursor = conn.cursor()
+
+    # Fech the conversation associated with the UUID
+    cursor.execute("SELECT id FROM conversations WHERE uuid = ?", (conversation_uuid,))
+    conversation_id = cursor.fetchone()[0]
+    if conversation_id is None:
+        return {"error": "Invalid conversation ID"}, 404
+    
+    # Validate all messages logic 
+    has_paid_subscription = get_user_has_subscription(token)
+    messages_count = get_user_messages_count(user_id)
+    if not has_paid_subscription and messages_count >= MESSAGE_LIMIT:
+        return {"error": "Exceded credit quota"}, 200
+
     cursor.execute(
         "INSERT INTO messages(conversation_id, user_id, type, message) VALUES(?,?,?,?)",
         (conversation_id, user_id, 1, request.json["message"]),
@@ -97,22 +92,34 @@ def preflight(user_id=None, conversation_id=None):
 
 
 @app.route(
-    "/api/chat/<int:user_id>/<int:conversation_id>/<int:message_id>/query",
+    "/api/chat/<conversation_uuid>/<int:message_id>/query",
     methods=["POST"],
 )
-def ask(user_id=None, conversation_id=None, message_id=None):
-    if user_id is None or conversation_id is None:
-        return {"error": "User ID is required"}, 400
+def ask(conversation_uuid=None, message_id=None):
+    if "Authorization" not in request.headers:
+        return {"status": False, "message": "Authorization header is required"}, 403
 
-    if not request.json:
-        return {"error": "Request body is required"}, 400
+    token = request.headers.get("Authorization").split(" ")[1]
+    payload = verify_jwt(token)
 
-    # TODO: Implement user validation
-    if not is_conversation_valid(conversation_id):
-        return {"error": "Invalid conversation ID"}, 400
+    if payload is not None:
+        user_id = payload["ur"]
+    else:
+        return {"status": False, "message": "Invalid user ID"}, 403
+
+    if not is_conversation_valid(conversation_uuid):
+        return {"error": "Invalid conversation ID"}, 403
+
+    # Validate all messages logic 
+    has_paid_subscription = get_user_has_subscription(token)
+    messages_count = get_user_messages_count(user_id)
+
+    if not has_paid_subscription and messages_count >= MESSAGE_LIMIT:
+        return {"error": "Exceded credit quota"}, 403
+    
 
     def retrieve_last_two_messages_as_chat_message(
-        conversation_id, user_id, message_id
+        conversation_uuid, user_id, message_id
     ):
         conn = get_db()
         cursor = conn.cursor()
@@ -120,20 +127,23 @@ def ask(user_id=None, conversation_id=None, message_id=None):
             """
                 SELECT id, conversation_id, message, type, DATETIME(created_at,'localtime') as created_at 
                 FROM messages 
-                WHERE conversation_id = ? 
+                WHERE conversation_id = (SELECT id FROM conversations WHERE uuid = ?)
                 AND user_id = ?
                 AND id < ?
                 ORDER BY id DESC LIMIT 10
             """,
-            (conversation_id, user_id, message_id),
+            (conversation_uuid, user_id, message_id),
         )
         messages = cursor.fetchall()
         if messages is None:
             return []
-        
+
         chat_history = []
         for message in messages:
-            print(message[2], MessageRole.USER if message[3] == 1 else MessageRole.ASSISTANT)
+            print(
+                message[2],
+                MessageRole.USER if message[3] == 1 else MessageRole.ASSISTANT,
+            )
             chat_history.append(
                 ChatMessage(
                     content=message[2],
@@ -152,7 +162,9 @@ def ask(user_id=None, conversation_id=None, message_id=None):
         )
         message = data["message"] if "message" in data else ""
 
-        chat_history = retrieve_last_two_messages_as_chat_message(conversation_id, user_id, message_id)
+        chat_history = retrieve_last_two_messages_as_chat_message(
+            conversation_uuid, user_id, message_id
+        )
 
         response_stream = chat_engine.stream_chat(message, chat_history=chat_history)
         for response in response_stream.response_gen:
@@ -177,14 +189,25 @@ def ask(user_id=None, conversation_id=None, message_id=None):
         content_type="charset=UTF-8",
         headers={"X-Accel-Buffering": "no"},
     )
-    # return chat_engine.chat(data["message"]).response
 
 
-@app.route("/api/chat/<int:user_id>", methods=["GET"])
-def get_chat(user_id=None):
+
+@app.route("/api/chat", methods=["GET"])
+def get_chat():
+    if "Authorization" not in request.headers:
+        return {"status": False, "message": "Authorization header is required"}, 403
+
+    token = request.headers.get("Authorization").split(" ")[1]
+    payload = verify_jwt(token)
+
+    if payload is not None:
+        user_id = payload["ur"]
+    else:
+        return {"status": False, "message": "Invalid user ID"}, 403
+
     cursor = get_db().cursor()
     cursor.execute(
-        "SELECT id, DATETIME(created_at,'localtime') as created_at FROM conversations WHERE user_id = ? ORDER BY created_at DESC",
+        "SELECT id, uuid, DATETIME(created_at,'localtime') as created_at FROM conversations WHERE user_id = ? ORDER BY created_at DESC",
         (user_id,),
     )
     conversations = cursor.fetchall()
@@ -193,32 +216,110 @@ def get_chat(user_id=None):
     return jsonify({"conversations": result})
 
 
-@app.route("/api/chat/<int:user_id>", methods=["POST"])
-def post_chat(user_id=None):
+@app.route("/api/chat", methods=["POST"])
+def post_chat():
+    if "Authorization" not in request.headers:
+        return {"status": False, "message": "Authorization header is required"}, 403
+
+    token = request.headers.get("Authorization").split(" ")[1]
+    payload = verify_jwt(token)
+
+    if payload is not None:
+        user_id = payload["ur"]
+    else:
+        return {"status": False, "message": "Invalid user ID"}, 403
+
     conn = get_db()
     cursor = conn.cursor()
     cursor.execute("INSERT INTO conversations(user_id) VALUES(?)", (user_id,))
     conn.commit()
-    print(cursor.lastrowid)
-    return jsonify({"conversation": cursor.lastrowid})
+    conversation_id = cursor.lastrowid
+    print(type(conversation_id), conversation_id)
+    cursor.execute(
+        "SELECT uuid FROM conversations WHERE id = ? ",
+        (conversation_id,),
+    )
+    conversations = cursor.fetchone()
+    return jsonify({"conversation": conversations[0]})
 
 
-@app.route("/api/chat/<int:user_id>/<int:conversation_id>/messages", methods=["GET"])
-def get_conversation(user_id=None, conversation_id=None):
-    # TODO: Implement user validation
-    if not is_conversation_valid(conversation_id):
-        return {"error": "Invalid conversation ID"}, 400
+@app.route("/api/chat/<conversation_uuid>", methods=["GET"])
+def get_conversation(conversation_uuid=None):
+    if "Authorization" not in request.headers:
+        return {"status": False, "message": "Authorization header is required"}, 403
+
+    token = request.headers.get("Authorization").split(" ")[1]
+    payload = verify_jwt(token)
+
+    if payload is not None:
+        user_id = payload["ur"]
+    else:
+        return {"status": False, "message": "Invalid user ID"}, 403
+
+    if not is_conversation_valid(conversation_uuid):
+        return {"error": "Invalid conversation ID"}, 403
 
     cursor = get_db().cursor()
     cursor.execute(
-        "SELECT id, conversation_id, message, type, DATETIME(created_at,'localtime') as created_at FROM messages WHERE conversation_id = ? and user_id = ? ORDER BY created_at ASC",
-        (conversation_id, user_id),
+        "SELECT uuid FROM conversations WHERE uuid = ? and user_id = ? ORDER BY created_at DESC",
+        (conversation_uuid, user_id),
+    )
+    conversation = cursor.fetchone()
+    if conversation is None or len(conversation) == 0:
+        return {"status": False, "message":  "Conversation is not valid"}, 403
+    
+    return jsonify({"conversation": conversation[0]})
+
+
+@app.route("/api/chat/<conversation_uuid>/messages", methods=["GET"])
+def get_conversation_messages(conversation_uuid=None):
+    if "Authorization" not in request.headers:
+        return {"status": False, "message": "Authorization header is required"}, 403
+
+    token = request.headers.get("Authorization").split(" ")[1]
+    payload = verify_jwt(token)
+
+    if payload is not None:
+        user_id = payload["ur"]
+    else:
+        return {"status": False, "message": "Invalid user ID"}, 403
+
+    if not is_conversation_valid(conversation_uuid):
+        return {"error": "Invalid conversation ID"}, 403
+
+    cursor = get_db().cursor()
+    cursor.execute(
+        """
+        SELECT id, conversation_id, message, type, DATETIME(created_at,'localtime') as created_at 
+        FROM messages 
+        WHERE conversation_id = (SELECT id FROM conversations WHERE uuid = ?) 
+        AND user_id = ? 
+        ORDER BY created_at ASC
+        """,
+        (conversation_uuid, user_id),
     )
     messages = cursor.fetchall()
     column_names = list(map(lambda x: x[0], cursor.description))
     result = [dict(zip(column_names, row)) for row in messages]
     print(messages)
     return jsonify({"messages": result})
+
+
+@app.route("/api/user/<int:user_id>", methods=["GET"]) 
+def get_message_count_from_user(user_id=None):
+    if user_id is None:
+        return {"status": False, "message": "User ID is required"}, 400
+
+    if "Authorization" not in request.headers:
+        return {"status": False, "message": "Authorization header is required"}, 403 
+    
+    token = request.headers.get("Authorization").split(" ")[1]
+    if token != API_KEY:
+        return {"status": False, "message": "Invalid API Key"}, 403
+    
+    message_count = get_user_messages_count(user_id)
+    return jsonify({"status": True, "message_count": message_count, "monthly_limit": MESSAGE_LIMIT})
+
 
 
 @app.route("/", methods=["GET"])
